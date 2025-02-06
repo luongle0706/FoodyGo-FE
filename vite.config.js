b67f3314a3a42cc9a5579760ac32657bd7eea56e
AnhNTSE183225 <anhntse183225@fpt.edu.vn>
2025-02-06 13:50:59 +0700

    test cicd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'admin.foodygo.theanh0804.id.vn', // Chạy server trên domain này
+    strictPort: true, // Đảm bảo chạy trên một cổng cố định
+    cors: true, // Cho phép CORS nếu cần
+    origin: 'https://admin.foodygo.theanh0804.id.vn', // Xác định nguồn gốc truy cập
+  },
 })
