a4a6c4c41b468dcfb5b01b5fa341203bef0f9c3b
AnhNT <anhntse183225@fpt.edu.vn>
2025-02-06 21:37:44 +0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'admin.foodygo.theanh0804.id.vn', // Chạy server trên domain này
-    strictPort: true, // Đảm bảo chạy trên một cổng cố định
-    cors: true, // Cho phép CORS nếu cần
-    origin: 'https://admin.foodygo.theanh0804.id.vn', // Xác định nguồn gốc truy cập
+    host: true
   },
 })
